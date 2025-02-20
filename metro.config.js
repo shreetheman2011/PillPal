9395bb610d5a1a55785c3b7006dcea6b35f605a0
shreetheman2011 <shree.manickaraja@gmail.com>
2025-02-19 16:45:41 -0800

    test

---
+++ metro.config.js
@@ -0,0 +1,14 @@
+const { getDefaultConfig } = require("metro-config");
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
+  return {
+    resolver: {
+      assetExts: assetExts.filter((ext) => ext !== "svg"),
+      sourceExts: [...sourceExts, "svg", "cjs"],
+      resolverMainFields: ["sbmodern", "browser", "main"],
+    },
+  };
+})();
